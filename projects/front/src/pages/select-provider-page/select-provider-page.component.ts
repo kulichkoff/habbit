a4511d056e697e695af45d5e11b007514caf6fd7
Daniel Kulichkov <danchick03@gmail.com>
2026-01-03 18:06:50 +0300

    feat(front): layout select provider page

--- projects/front/src/pages/select-provider-page/select-provider-page.component.ts
+++ projects/front/src/pages/select-provider-page/select-provider-page.component.ts
@@ -1,9 +1,30 @@
-import { Component } from '@angular/core';
+import { first } from 'rxjs';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { Store } from '@ngxs/store';
+
+import { SetStorageProvider, StorageProvider } from '@front/entities/settings';
 
 @Component({
   selector: 'app-select-provider-page',
-  imports: [],
+  imports: [MatButtonModule, MatTooltipModule],
   templateUrl: './select-provider-page.component.html',
   styleUrl: './select-provider-page.component.scss',
 })
-export class SelectProviderPageComponent {}
+export class SelectProviderPageComponent {
+  private readonly store = inject(Store);
+  private readonly router = inject(Router);
+
+  protected StorageProvider = StorageProvider;
+
+  protected chooseProvider(provider: StorageProvider) {
+    this.store
+      .dispatch(new SetStorageProvider(provider))
+      .pipe(first())
+      .subscribe(() => {
+        this.router.navigate(['checker']);
+      });
+  }
+}
