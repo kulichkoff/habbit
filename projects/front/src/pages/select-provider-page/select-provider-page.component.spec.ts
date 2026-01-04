f295dfdaf1d2e9d55b1989933db1e8315c5d9d17
Daniel Kulichkov <danchick03@gmail.com>
2026-01-04 21:11:05 +0300

    test(front): fix omponent tests

--- projects/front/src/pages/select-provider-page/select-provider-page.component.spec.ts
+++ projects/front/src/pages/select-provider-page/select-provider-page.component.spec.ts
@@ -2,6 +2,8 @@ import { describe, beforeEach, it, expect } from 'vitest';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SelectProviderPageComponent } from './select-provider-page.component';
+import { provideStore } from '@ngxs/store';
+import { SettingsState } from '@front/entities/settings';
 
 describe('SelectProviderPageComponent', () => {
   let component: SelectProviderPageComponent;
@@ -10,6 +12,7 @@ describe('SelectProviderPageComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [SelectProviderPageComponent],
+      providers: [provideStore([SettingsState])],
     }).compileComponents();
 
     fixture = TestBed.createComponent(SelectProviderPageComponent);
