1ff8ce8fbb77af5558b3480d3b837964f44b7214
Daniel Kulichkov <danchick03@gmail.com>
2026-01-06 14:49:59 +0300

    feat(front/checker): disable today check in

--- projects/front/src/widgets/habit-dashboard/ui/habit-dashboard/habit-dashboard.component.ts
+++ projects/front/src/widgets/habit-dashboard/ui/habit-dashboard/habit-dashboard.component.ts
@@ -1,11 +1,12 @@
-import { ChangeDetectionStrategy, Component, inject, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject, input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatIconModule } from '@angular/material/icon';
 import { CheckerBoardComponent } from '@front/entities/checker-board';
-import { BoardModel } from '@front/entities/habit-track';
+import { BoardModel, TrackType } from '@front/entities/habit-track';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { CheckInService } from '@front/features/check-in';
+import { compareCalendarDate } from '@front/shared/date';
 
 @Component({
   selector: 'app-habit-dashboard',
@@ -24,6 +25,14 @@ export class HabitDashboardComponent {
   private readonly checkInService = inject(CheckInService);
 
   board = input.required<BoardModel>();
+
+  protected hasCheckedToday = computed<boolean>(() =>
+    this.board().trackedList.some(check => compareCalendarDate(check.checkDate, new Date()) === 0),
+  );
+
+  protected disabledTodayCheckIn = computed<boolean>(
+    () => this.hasCheckedToday() && this.board().type !== TrackType.MultiCheck,
+  );
 
   protected checkInToday(): void {
     this.checkIn(new Date());
