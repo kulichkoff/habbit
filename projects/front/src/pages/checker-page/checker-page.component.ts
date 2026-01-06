ec59276167e6d2c68367556f7f76c998e302c49b
Daniel Kulichkov <danchick03@gmail.com>
2026-01-06 20:12:51 +0300

    feat(front/checker): move boards list to sidenav

--- projects/front/src/pages/checker-page/checker-page.component.ts
+++ projects/front/src/pages/checker-page/checker-page.component.ts
@@ -1,13 +1,14 @@
 import { Component, inject } from '@angular/core';
 import { Store } from '@ngxs/store';
 import { AsyncPipe } from '@angular/common';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { BoardsState } from '@front/entities/habit-track';
 import { HabitDashboardComponent } from '@front/widgets/habit-dashboard';
 import { BoardsListComponent } from '@front/widgets/boards-list';
 
 @Component({
   selector: 'app-checker-page',
-  imports: [AsyncPipe, BoardsListComponent, HabitDashboardComponent],
+  imports: [AsyncPipe, MatSidenavModule, BoardsListComponent, HabitDashboardComponent],
   templateUrl: './checker-page.component.html',
   styleUrl: './checker-page.component.scss',
 })
