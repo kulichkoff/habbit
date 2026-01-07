6abcd859dd2b68deb6a6e7cbdca9327e5353bdc6
Daniel Kulichkov <danchick03@gmail.com>
2026-01-07 13:31:36 +0300

    feat(front): add header and mobile observer

--- projects/front/src/pages/checker-page/checker-page.component.ts
+++ projects/front/src/pages/checker-page/checker-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject, signal } from '@angular/core';
-import { toSignal } from '@angular/core/rxjs-interop';
+import { takeUntilDestroyed, toSignal } from '@angular/core/rxjs-interop';
 import { Store } from '@ngxs/store';
 import { AsyncPipe } from '@angular/common';
 import { MatSidenavModule } from '@angular/material/sidenav';
@@ -10,6 +10,8 @@ import { map } from 'rxjs';
 import { EmptyContentComponent } from '@front/shared/ui';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-checker-page',
@@ -18,6 +20,7 @@ import { MatButtonModule } from '@angular/material/button';
     MatSidenavModule,
     MatIconModule,
     MatButtonModule,
+    MatToolbarModule,
     BoardsListComponent,
     HabitDashboardComponent,
     EmptyContentComponent,
@@ -27,6 +30,12 @@ import { MatButtonModule } from '@angular/material/button';
 })
 export class CheckerPageComponent {
   private readonly store = inject(Store);
+  private readonly breakpointObserver = inject(BreakpointObserver);
+
+  protected isMobile$ = this.breakpointObserver.observe(Breakpoints.XSmall).pipe(
+    takeUntilDestroyed(),
+    map(state => state.matches),
+  );
 
   protected selectedBoard$ = this.store.select(BoardsState.getSelectedBoard);
   protected havingBoards = toSignal(
